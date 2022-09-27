cbb6447ec5bf5a0b95b3d114043590fd25ffb620
Moringa School <Abdulbasid.sheikh@student.moringaschool>
2022-09-27 12:11:25 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record){
+    const found = record.find(log => {
+        if(log.result === "W"){
+            return log;
+        }
+    }) 
+    if(found){
+        return found.year;
+      }else{
+        return found
+      }
+  }
